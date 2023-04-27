be6a02289df34cf4eb6f1afd1a0e8efb1a0cc665
pc <pc@DESKTOP-NJJC3PI>
2023-04-27 15:24:17 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature
+    {
+        public int FeatureID { get; set; }
+        public string FirstPostTitle { get; set; }
+        public string FirstPostDescription { get; set; }
+        public string FirstPostImage { get; set; }
+        public bool FirstPostStatus { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/SecondFeature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class SecondFeature
+    {
+        public int SecondFeatureID { get; set; }
+        public string SecondtPostTitle { get; set; }
+        public string SecondPostDescription { get; set; }
+        public string SecondPostImage { get; set; }
+        public bool SecondPostStatus { get; set; }
+    }
+}
